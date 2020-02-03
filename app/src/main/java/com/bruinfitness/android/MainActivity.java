966dc10a884838dd1a0ff85688a78f5acfcc53fc
thomasmburke <tburke@bu.edu>
2020-02-02 21:09:49 -0800

    added calendar view and changed primary colors

--- app/src/main/java/com/bruinfitness/android/MainActivity.java
+++ app/src/main/java/com/bruinfitness/android/MainActivity.java
@@ -3,6 +3,13 @@ package com.bruinfitness.android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+import devs.mulham.horizontalcalendar.HorizontalCalendar;
+import devs.mulham.horizontalcalendar.HorizontalCalendarView;
+import devs.mulham.horizontalcalendar.utils.HorizontalCalendarListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Calendar endDate = Calendar.getInstance();
+        endDate.add(Calendar.WEEK_OF_MONTH, 1);
+        Calendar startDate = Calendar.getInstance();
+        startDate.add(Calendar.WEEK_OF_MONTH, -1);
+
+        HorizontalCalendar horizontalCalendar = new HorizontalCalendar.Builder(this, R.id.calendarView)
+                .range(startDate, endDate)
+                .datesNumberOnScreen(5)
+                .configure()
+                .showTopText(false)
+                .textSize(12, 12, 12)
+                .end()
+                .build();
+
+
+        horizontalCalendar.setCalendarListener(new HorizontalCalendarListener() {
+            @Override
+            //public void onDateSelected(Calendar date, int position) {
+            public void onDateSelected(Calendar date, int position) {
+                //Toast.makeText(MainActivity.this, DateFormat.getDateInstance().format(date) + " is selected!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "calendar click!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCalendarScroll(HorizontalCalendarView calendarView,
+                                         int dx, int dy) {
+                //Toast.makeText(MainActivity.this, "calendar scroll", Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public boolean onDateLongClicked(Calendar date, int position) {
+                Toast.makeText(MainActivity.this, "calendar long click!", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
     }
 }
