d0ef161afbfe7f756850e452b5de94ebe933bfc4
thomasmburke <tburke@bu.edu>
2020-02-23 20:44:56 -0800

    Firestore toObject

--- app/src/main/java/com/bruinfitness/android/MainActivity.java
+++ app/src/main/java/com/bruinfitness/android/MainActivity.java
@@ -14,11 +14,19 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collection;
 import java.util.List;
 
 import devs.mulham.horizontalcalendar.HorizontalCalendar;
@@ -29,6 +37,9 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
     public SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy_MM_dd");
+    // Access a Cloud Firestore instance from your Activity
+    private CollectionReference firestoreDb = FirebaseFirestore.getInstance().collection("workouts");
+    //private CollectionReference firestoreDb = FirebaseFirestore.getInstance().collection("test");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +51,38 @@ public class MainActivity extends AppCompatActivity {
         endDate.add(Calendar.WEEK_OF_MONTH, 1);
         Calendar startDate = Calendar.getInstance();
         startDate.add(Calendar.WEEK_OF_MONTH, -1);
+        Calendar tmpDate = Calendar.getInstance();
+        tmpDate.add(Calendar.WEEK_OF_MONTH, 1);
+
+        while (!getDateString(tmpDate).equals(getDateString(endDate))){
+            Log.i(TAG, getDateString(tmpDate));
+
+            tmpDate.add(Calendar.DAY_OF_YEAR, 1);
+        }
+        Log.i(TAG, "startDate after loop=" + getDateString(startDate));
+        Task<QuerySnapshot> workoutsfortheday = firestoreDb.document("2020-02-17").collection("types")
+        //Task<QuerySnapshot> workoutsfortheday = firestoreDb
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    //Workout workoutObject= task.getResult().getDocuments().get(0).toObject(Workout.class);
+                    Log.d(TAG, "test");
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        Log.d(TAG, document.getId() + " => " + document.getData());
+                        //deleteme foo = document.toObject(deleteme.class);
+                        Workout w1 = document.toObject(Workout.class);
+                        //String description = w1.getDescription();
+                        Log.d(TAG, "test");
+                    }
+                    Log.d(TAG, "test");
+                } else {
+                    Log.d(TAG, "Error getting documents: ", task.getException());
+                }
+            }
+        });
+
 
         HorizontalCalendar horizontalCalendar = new HorizontalCalendar.Builder(this, R.id.calendarView)
                 .range(startDate, endDate)
@@ -67,7 +110,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public boolean onDateLongClicked(Calendar date, int position) {
-                Toast.makeText(MainActivity.this, "calendar long click!", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(MainActivity.this, "calendar long click!", Toast.LENGTH_SHORT).show();
                 return true;
             }
         });
--- app/src/main/java/com/bruinfitness/android/Workout.java
+++ app/src/main/java/com/bruinfitness/android/Workout.java
@@ -1,5 +1,7 @@
 package com.bruinfitness.android;
 
+import com.google.firebase.firestore.Exclude;
+
 /**
  * {@link Workout} represents a type of workout that the user wants to learn.
  * It contains resource IDs for the default translation, Miwok translation, audio file, and
@@ -9,16 +11,16 @@ package com.bruinfitness.android;
 public class Workout {
 
     /** String type of workout i.e. crossfit, gymnastics, weightlifting */
-    private String mWorkoutType;
+    private String workoutType;
 
     /** Name of the workout i.e. Diane, Murph */
-    private String mName;
+    private String name;
 
     /** Explains the goal of the workout */
-    private String mGoal;
+    private String goal;
 
     /** Workout contents */
-    private String mDescription;
+    private String description;
 
     /** State of the item */
     private boolean mExpanded;
@@ -32,38 +34,51 @@ public class Workout {
      * @param description Workout contents
      */
     public Workout(String workoutType, String name, String goal, String description) {
-        mWorkoutType = workoutType;
-        mName = name;
-        mGoal = goal;
-        mDescription = description;
+        this.workoutType = workoutType;
+        this.name = name;
+        this.goal = goal;
+        this.description = description;
     }
+
+    public Workout(String name, String goal, String description) {
+        this.name = name;
+        this.goal = goal;
+        this.description = description;
+    }
+
+
+    public Workout(){
+        //public no-arg constructor needed
+    }
+
+
 
     /**
      * Get the string workoutType string.
      */
     public String getWorkoutType() {
-        return mWorkoutType;
+        return this.workoutType;
     }
 
     /**
      * Get the specific name of the workout.
      */
     public String getName() {
-        return mName;
+        return this.name;
     }
 
     /**
      * Return the goal of the workout.
      */
     public String getGoal() {
-        return mGoal;
+        return this.goal;
     }
 
     /**
      * Return the contents of the workout
      */
     public String getDescription() {
-        return mDescription;
+        return this.description;
     }
 
     public void setExpanded(boolean expanded) {
@@ -73,4 +88,18 @@ public class Workout {
     public boolean isExpanded() {
         return mExpanded;
     }
+
+    public void setWorkoutType(String workoutType){
+        this.workoutType = workoutType;
+    }
+    public void setGoal(String goal){
+        this.goal = goal;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setDescription(String description){
+        this.description = description;
+    }
+
 }
