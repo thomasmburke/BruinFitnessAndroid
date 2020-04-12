b7726d6b1bbc29a5a75e591c9289bb5d51310f8b
thomasmburke <tburke@bu.edu>
2020-04-12 10:22:15 -0700

    added basic authentication flow

---
+++ app/src/main/java/com/bruinfitness/android/LoginActivity.java
@@ -0,0 +1,58 @@
+package com.bruinfitness.android;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+
+public class LoginActivity extends AppCompatActivity {
+    // Choose an arbitrary request code value
+    private static final int RC_SIGN_IN = 123;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        showSignInOptions();
+    }
+
+    private void showSignInOptions(){
+        startActivityForResult(
+                // Get an instance of AuthUI based on the default app
+                AuthUI.getInstance().
+                        createSignInIntentBuilder()
+                        .setAvailableProviders(Arrays.asList(
+                                new AuthUI.IdpConfig.EmailBuilder().build()))
+                        // TODO set theme here .setTheme(R.style.MyTheme)
+                        // TODO set Bruin Fitness logo here .setLogo(R.drawable.firebase)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if (resultCode == RESULT_OK) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                startActivity(new Intent(this, MainActivity.class));
+                finish();
+                Toast.makeText(this, "Welcome! " + user.getDisplayName(), Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "" + response.getError().getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+}
--- app/src/main/java/com/bruinfitness/android/SplashActivity.java
+++ app/src/main/java/com/bruinfitness/android/SplashActivity.java
@@ -2,17 +2,30 @@ package com.bruinfitness.android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            // user already signed in
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        } else {
+            // not signed in
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
 
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
+
     }
 }
